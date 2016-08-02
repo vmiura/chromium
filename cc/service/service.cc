8ec31c7d614008ed55bae033c3330f97916991f3
Bo Liu <boliu@chromium.org>
2016-08-02 14:05:09 -0700

    implement easy methods on ClientImpl
    Mainly copied directly from ProxyImpl

--- cc/service/service.cc
+++ cc/service/service.cc
@@ -12,61 +12,128 @@ namespace cc {
 class Service::ClientImpl : public LayerTreeHostImplClient,
                             public SchedulerClient {
  public:
-  explicit ClientImpl(Service* owner) : owner_(owner) {}
+  ClientImpl(Service* owner,
+             const scoped_refptr<base::SingleThreadTaskRunner>& task_runner)
+      : owner_(owner), task_runner_(task_runner) {}
 
   // LayerTreeHostImplClient implementation.
-  void UpdateRendererCapabilitiesOnImplThread() override {}
-  void DidLoseOutputSurfaceOnImplThread() override {}
+  void UpdateRendererCapabilitiesOnImplThread() override {
+    // TODO(hackathon): back to main
+  }
+  void DidLoseOutputSurfaceOnImplThread() override {
+    // TODO(hackathon): back to main
+  }
   void CommitVSyncParameters(base::TimeTicks timebase,
                              base::TimeDelta interval) override {}
-  void SetBeginFrameSource(BeginFrameSource* source) override {}
-  void SetEstimatedParentDrawTime(base::TimeDelta draw_time) override {}
-  void DidSwapBuffersCompleteOnImplThread() override {}
-  void OnCanDrawStateChanged(bool can_draw) override {}
-  void NotifyReadyToActivate() override {}
-  void NotifyReadyToDraw() override {}
-  // Please call these 3 functions through
-  // LayerTreeHostImpl's SetNeedsRedraw(), SetNeedsRedrawRect() and
-  // SetNeedsOneBeginImplFrame().
-  void SetNeedsRedrawOnImplThread() override {}
-  void SetNeedsRedrawRectOnImplThread(const gfx::Rect& damage_rect) override {}
-  void SetNeedsOneBeginImplFrameOnImplThread() override {}
-  void SetNeedsCommitOnImplThread() override {}
-  void SetNeedsPrepareTilesOnImplThread() override {}
-  void SetVideoNeedsBeginFrames(bool needs_begin_frames) override {}
+  void SetBeginFrameSource(BeginFrameSource* source) override {
+    owner_->scheduler()->SetBeginFrameSource(source);
+  }
+  void SetEstimatedParentDrawTime(base::TimeDelta draw_time) override {
+    owner_->scheduler()->SetEstimatedParentDrawTime(draw_time);
+  }
+  void DidSwapBuffersCompleteOnImplThread() override {
+    owner_->scheduler()->DidSwapBuffersComplete();
+    // TODO(hackathon): back to main
+  }
+  void OnCanDrawStateChanged(bool can_draw) override {
+    owner_->scheduler()->SetCanDraw(can_draw);
+  }
+  void NotifyReadyToActivate() override {
+    owner_->scheduler()->NotifyReadyToActivate();
+  }
+  void NotifyReadyToDraw() override {
+    owner_->scheduler()->NotifyReadyToDraw();
+  }
+  void SetNeedsRedrawOnImplThread() override {
+    owner_->scheduler()->SetNeedsRedraw();
+  }
+  void SetNeedsRedrawRectOnImplThread(const gfx::Rect& damage_rect) override {
+    owner_->host_impl()->SetViewportDamage(damage_rect);
+    SetNeedsRedrawOnImplThread();
+  }
+  void SetNeedsOneBeginImplFrameOnImplThread() override {
+    owner_->scheduler()->SetNeedsOneBeginImplFrame();
+  }
+  void SetNeedsCommitOnImplThread() override {
+    owner_->scheduler()->SetNeedsBeginMainFrame();
+  }
+  void SetNeedsPrepareTilesOnImplThread() override {
+    owner_->scheduler()->SetNeedsPrepareTiles();
+  }
+  void SetVideoNeedsBeginFrames(bool needs_begin_frames) override {
+    owner_->scheduler()->SetVideoNeedsBeginFrames(needs_begin_frames);
+  }
   void PostAnimationEventsToMainThreadOnImplThread(
-      std::unique_ptr<AnimationEvents> events) override {}
-  bool IsInsideDraw() override { return false; }
-  void RenewTreePriority() override {}
+      std::unique_ptr<AnimationEvents> events) override {
+    // TODO(hackathon): back to main
+  }
+  bool IsInsideDraw() override {
+    // TODO(hackathon): implement.
+    return false;
+  }
+  void RenewTreePriority() override {
+    // TODO(hackathon): implement.
+  }
   void PostDelayedAnimationTaskOnImplThread(const base::Closure& task,
-                                            base::TimeDelta delay) override {}
-  void DidActivateSyncTree() override {}
-  void WillPrepareTiles() override {}
-  void DidPrepareTiles() override {}
-  void DidCompletePageScaleAnimationOnImplThread() override {}
-  void OnDrawForOutputSurface(bool resourceless_software_draw) override {}
+                                            base::TimeDelta delay) override {
+    task_runner_->PostDelayedTask(FROM_HERE, task, delay);
+  }
+  void DidActivateSyncTree() override {
+    // TODO(hackathon): send sync ipc reply back to main (?)
+  }
+  void WillPrepareTiles() override {
+    owner_->scheduler()->WillPrepareTiles();
+  }
+  void DidPrepareTiles() override {
+    owner_->scheduler()->DidPrepareTiles();
+  }
+  void DidCompletePageScaleAnimationOnImplThread() override {
+    // TODO(hackathon): back to main
+  }
+  void OnDrawForOutputSurface(bool resourceless_software_draw) override {
+    NOTREACHED(); // webivew only
+  }
 
   // SchedulerClient implementation.
-  void WillBeginImplFrame(const BeginFrameArgs& args) override {}
-  void ScheduledActionSendBeginMainFrame(const BeginFrameArgs& args) override {}
+  void WillBeginImplFrame(const BeginFrameArgs& args) override {
+    owner_->host_impl()->WillBeginImplFrame(args);
+  }
+  void ScheduledActionSendBeginMainFrame(const BeginFrameArgs& args) override {
+    // TODO(hackathon): back to main
+  }
   DrawResult ScheduledActionDrawAndSwapIfPossible() override {
+    // TODO(hackathon): implement.
     return DRAW_SUCCESS;
   }
   DrawResult ScheduledActionDrawAndSwapForced() override {
+    // TODO(hackathon): implement.
     return DRAW_SUCCESS;
   }
-  void ScheduledActionCommit() override {}
-  void ScheduledActionActivateSyncTree() override {}
+  void ScheduledActionCommit() override {
+    // TODO(hackathon): implement.
+  }
+  void ScheduledActionActivateSyncTree() override {
+    owner_->host_impl()->ActivateSyncTree();
+  }
   void ScheduledActionBeginOutputSurfaceCreation() override {
     owner_->CreateOutputSurface();
   }
-  void ScheduledActionPrepareTiles() override {}
-  void ScheduledActionInvalidateOutputSurface() override {}
-  void DidFinishImplFrame() override {}
-  void SendBeginMainFrameNotExpectedSoon() override {}
+  void ScheduledActionPrepareTiles() override {
+    owner_->host_impl()->PrepareTiles();
+  }
+  void ScheduledActionInvalidateOutputSurface() override {
+    NOTREACHED(); // webivew only
+  }
+  void DidFinishImplFrame() override {
+    owner_->host_impl()->DidFinishImplFrame();
+  }
+  void SendBeginMainFrameNotExpectedSoon() override {
+    // TODO(hackathon): back to main.
+  }
 
  private:
-  Service* owner_;
+  Service* const owner_;
+  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
 
   DISALLOW_COPY_AND_ASSIGN(ClientImpl);
 };
@@ -76,7 +143,7 @@ Service::Service(cc::mojom::CompositorRequest request,
                  SharedBitmapManager* shared_bitmap_manager,
                  gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager,
                  TaskGraphRunner* task_graph_runner)
-    : client_(new ClientImpl(this)),
+    : client_(new ClientImpl(this, base::ThreadTaskRunnerHandle::Get())),
       task_runner_(base::ThreadTaskRunnerHandle::Get()),
       scheduler_(client_.get(),
                  SchedulerSettings(),
